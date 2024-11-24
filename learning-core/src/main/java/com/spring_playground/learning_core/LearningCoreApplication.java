3bc8d7a28905e4c9a3847ee917ec6aeaa80e4e38
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2024-11-24 16:40:36 +0700

    feat : init project for learning core spring

---
+++ learning-core/src/main/java/com/spring_playground/learning_core/LearningCoreApplication.java
@@ -0,0 +1,13 @@
+package com.spring_playground.learning_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningCoreApplication.class, args);
+	}
+
+}
