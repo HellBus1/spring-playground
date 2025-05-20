db85195471d8d9c22a1c1ca6ba73fbb7fa81bee0
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2025-05-20 21:25:21 +0700

    initialize project

---
+++ database-exploration/src/test/java/com/spring_playground/database_exploration/DatabaseExplorationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_playground.database_exploration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseExplorationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
